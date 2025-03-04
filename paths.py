2dd0be09c4b8c397f2b9f71552241aa532ffb6f8
sbulusu123 <120585728+sbulusu123@users.noreply.github.com>
2025-03-03 19:49:41 -0800

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,9 +1,9 @@
-raw_training_data_folder = "/media/fabian/My Book/datasets/BraTS/2017/Brats17TrainingData"
-raw_validation_data_folder = "/media/fabian/My Book/datasets/BraTS/2017/Brats17ValidationData"
-raw_testing_data_folder = "/media/fabian/My Book/datasets/BraTS/2017/Brats17TestingData"
+raw_training_data_folder = ""C:\Users\sbulu\OneDrive\Desktop\ssdata\training_data""
+raw_validation_data_folder = ""C:\Users\sbulu\OneDrive\Desktop\ssdata\validation-data""
+raw_testing_data_folder = ""C:\Users\sbulu\OneDrive\Desktop\ssdata\testing_data""
 
-preprocessed_training_data_folder = "/media/fabian/DeepLearningData/datasets/BraTS_2017"
-preprocessed_validation_data_folder = "/media/fabian/DeepLearningData/datasets/BraTS_2017_val"
-preprocessed_testing_data_folder = "/media/fabian/DeepLearningData/datasets/BraTS_2017_test"
+preprocessed_training_data_folder = ""C:\Users\sbulu\OneDrive\Desktop\ssdata\preprocessed_training_data""
+preprocessed_validation_data_folder = ""C:\Users\sbulu\OneDrive\Desktop\ssdata\preprocessed_validation_data""
+preprocessed_testing_data_folder = ""C:\Users\sbulu\OneDrive\Desktop\ssdata\preprocessed_testing_data""
 
-results_folder = "/home/fabian/PhD/results/BraTS_2017_lasagne/"  # where to save the network training and validation files
+results_folder = ""C:\Users\sbulu\OneDrive\Desktop\ssdata\results_folder""  # where to save the network training and validation files
